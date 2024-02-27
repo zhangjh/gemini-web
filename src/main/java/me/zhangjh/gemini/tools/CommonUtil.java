cf371327c0f6a01e5749a895d7fc96810a9a18dd
njhxzhangjh <njhxzhangjihong@126.com>
2024-02-27 22:36:41 +0800

    转移markdown以便正确tts

--- src/main/java/me/zhangjh/gemini/tools/CommonUtil.java
+++ src/main/java/me/zhangjh/gemini/tools/CommonUtil.java
@@ -1,6 +1,15 @@
 package me.zhangjh.gemini.tools;
 
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.options.MutableDataSet;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.safety.Whitelist;
 
 import javax.sound.sampled.*;
 
@@ -54,5 +63,36 @@ public class CommonUtil {
         microphone.open(format, microphone.getBufferSize());
         microphone.start();
         return microphone;
+    }
+
+    public static String markdown2Html(String markdown) {
+        if(StringUtils.isEmpty(markdown)) {
+            return "";
+        }
+        MutableDataSet dataSet = new MutableDataSet();
+        Parser parser = Parser.builder(dataSet).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(dataSet).build();
+        Node document = parser.parse(markdown);
+        return renderer.render(document);
+    }
+
+    public static String html2Text(String html) {
+        if(StringUtils.isEmpty(html)) {
+            return "";
+        }
+        Document document = Jsoup.parse(html);
+        Document.OutputSettings outputSettings = new Document.OutputSettings().prettyPrint(false);
+        document.outputSettings(outputSettings);
+        document.select("br").append("\\n");
+        document.select("p").prepend("\\n");
+        document.select("p").append("\\n");
+        String newHtml = document.html().replaceAll("\\\\n", "\n");
+        String plainText = Jsoup.clean(newHtml, "", Whitelist.none(), outputSettings);
+        return StringEscapeUtils.unescapeHtml(plainText.trim());
+    }
+
+    public static String markdown2Text(String markdown) {
+        String text = html2Text(markdown2Html(markdown));
+        return text.replaceAll("\\*", "");
     }
 }
